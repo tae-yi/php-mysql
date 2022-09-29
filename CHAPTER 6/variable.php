ab4256120cbc2a1fe7593e8d5f0434ab84fa9fcc
Tae Yi Im <mintchodan@naver.com>
2022-09-29 16:57:28 +0900

    PHP의 변수

---
+++ CHAPTER 6/variable.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Variable</h1>
+        <?php
+        echo 1 + 1;
+        ?>
+        <h1>Variable</h1>
+        <?php
+        $a = 1;
+        echo $a + 1;
+        ?>
+        <h1>Variable</h1>
+        <?php
+        $b = 10;
+        echo $b + 1;
+        ?>
+        <h1>Variable</h1>
+        <?php
+        $name = "egoing";
+        echo "Lorem ipsum dolor sit amet, consectetur ".$name." adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aloqua. Ut enum ad minim veniam, quis nostrud exercitation ullamco ".$name." laboris nisi ut aliquip ex ea commodo conseguat.Duis aute irure dolor in reprehendrit in voluptate velit esse cillum dolore ".$name." eu fugiat nalla pariatur. Exvepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. by ".$name;
+        ?>
+        <h1>Variable</h1>
+        <?php
+        $name = "leezche";
+        echo "Lorem ipsum dolor sit amet, consectetur ".$name." adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aloqua. Ut enum ad minim veniam, quis nostrud exercitation ullamco ".$name." laboris nisi ut aliquip ex ea commodo conseguat.Duis aute irure dolor in reprehendrit in voluptate velit esse cillum dolore ".$name." eu fugiat nalla pariatur. Exvepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. by ".$name;
+        ?>
+    </body>
+</html>
