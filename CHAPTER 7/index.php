aa9040d105b8622fda9b657f2c51ec75602b754d
Tae Yi Im <mintchodan@naver.com>
2022-09-29 16:58:18 +0900

    PHP의 URL 파라미터

---
+++ CHAPTER 7/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>WEB</h1>
+        <ol>
+            <li><a href="index.php?id=HTML">HTML</a></li>
+            <li><a href="index.php?id=CSS">CSS</a></li>
+            <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>
+            <?php
+            echo $_GET['id'];
+            ?>
+        </h2>
+        Lorem ipsum dolor sit amet, consectetur egoing adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aloqua. Ut enum ad minim veniam, quis nostrud exercitation ullamco egoing laboris nisi ut aliquip ex ea commodo conseguat.Duis aute irure dolor in reprehendrit in voluptate velit esse cillum dolore egoing eu fugiat nalla pariatur. Exvepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. by egoing
+    </body>
+</html>
